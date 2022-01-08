9b9ca43d7c99065192f7e0e6d1fe690e52066887
totoinu <oztodog@gmail.com>
2022-01-08 16:59:40 +0900

    create loading component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 // import { View, Text, TextInput } from "react-native";
 // eslint-disable-next-line
-import styled, { css } from "@emotion/native";
+import styled, { css } from '@emotion/native';
 import { string, func } from 'prop-types';
+
+export default function Button(props) {
+  const { label, onPress, style } = props;
+  return (
+    <ButtonContainer
+      onPress={onPress}
+      style={css`
+        ${style}
+      `}
+    >
+      <ButtonLabel>{label}</ButtonLabel>
+    </ButtonContainer>
+  );
+}
+
+Button.propTypes = {
+  label: string.isRequired,
+  onPress: func,
+  style: string,
+};
+
+Button.defaultProps = {
+  onPress: null,
+  style: '',
+};
 
 const ButtonContainer = styled.TouchableOpacity`
   background-color: #467fd3;
@@ -18,21 +43,3 @@ const ButtonLabel = styled.Text`
   padding: 8px 32px;
   color: #fff;
 `;
-
-export default function Button(props) {
-  const { label, onPress } = props;
-  return (
-    <ButtonContainer onPress={onPress}>
-      <ButtonLabel>{ label }</ButtonLabel>
-    </ButtonContainer>
-  );
-}
-
-Button.propTypes = {
-  label: string.isRequired,
-  onPress: func,
-};
-
-Button.defaultProps = {
-  onPress: null,
-};
---
+++ src/components/Loading.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import styled from '@emotion/native';
+import { bool } from 'prop-types';
+
+export default function Loading(props) {
+  const { isLoading } = props;
+  if (!isLoading) {
+    return null;
+  }
+  return (
+    <Container>
+      <Inner>
+        <ActivityIndicator size="large" color="#000" />
+      </Inner>
+    </Container>
+  );
+}
+
+Loading.propTypes = {
+  isLoading: bool,
+};
+
+Loading.defaultProps = {
+  isLoading: false,
+};
+
+const Container = styled.View`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.5);
+  z-index: 5;
+`;
+
+const Inner = styled.View`
+  margin-bottom: 80px;
+`;
